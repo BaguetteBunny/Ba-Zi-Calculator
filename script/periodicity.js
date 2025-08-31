8cc1436aaad7675ec28fab2c0f3a38cc9328929c
Louis Billaut <ragedfear4875@gmail.com>
2025-09-01 01:15:30 +0800

    improve algorithm

--- script/periodicity.js
+++ script/periodicity.js
@@ -1,12 +1,15 @@
+// https://en.wikipedia.org/wiki/Four_Pillars_of_Destiny#Periodicity_of_Four_Pillars
+// Algorithm to solve the iso-Gregorian quadruplet
+
 function solveDiophantineForPeriodicity(max) {
     const solutions = [];
     for (let w = 0; w <= max; w++) {
         for (let x = 0; x <= max; x++) {
             for (let y = 0; y <= max; y++) {
-                for (let z = 0; z <= max; z++) {
-                    const lhs = 21900 * w + 60 * x;
-                    const rhs = 365 * y + 366 * z;
-                    if (lhs === rhs) {
+                const n = 21900 * w + 60 * x - 365 * y;
+                if (n % 366 === 0) {
+                    const z = n / 366;
+                    if (z >= 0 && z <= max) {
                         const solution = { w, x, y, z };
                         solutions.push(solution);
                     }
@@ -14,5 +17,7 @@ function solveDiophantineForPeriodicity(max) {
             }
         }
     }
-    return solutions
+    return solutions.splice(1); // Remove w = x = y = z = 0 because redundant
 }
+
+console.log(solveDiophantineForPeriodicity(500))
